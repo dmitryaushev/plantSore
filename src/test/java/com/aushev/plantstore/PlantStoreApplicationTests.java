ceef8320cf7e730feeb45f64c10c265f4ec9ae55
Aushev <dmitryaushev@gmail.com>
2020-05-09 15:05:36 +0300

    Initial commit

---
+++ src/test/java/com/aushev/plantstore/PlantStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aushev.plantstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
