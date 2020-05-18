f2ee5760919c8680ed8f7a00e03f665f80a66bb5
Aushev <dmitryaushev@gmail.com>
2020-05-19 00:08:17 +0300

    Update DDL script

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -1,7 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS public;
 
-CREATE DATABASE PLANT_STORE WITH OWNER postgres;
-ALTER SCHEMA public OWNER TO postgres;
+CREATE DATABASE plant_store;
 
 create table role
 (
@@ -9,8 +8,6 @@ create table role
     title varchar(10),
     primary key (id)
 );
-alter table role
-    owner to postgres;
 
 create table users
 (
@@ -24,8 +21,6 @@ create table users
     foreign key (role_id)
         references role (id)
 );
-alter table users
-    owner to postgres;
 
 create table manufacturer
 (
@@ -33,21 +28,17 @@ create table manufacturer
     title varchar(20) unique,
     primary key (id)
 );
-alter table manufacturer
-    owner to postgres;
 
 create table product
 (
     id              uuid,
-    title           varchar(20),
+    title           varchar(20) unique ,
     price           decimal,
     manufacturer_id uuid,
     primary key (id),
     foreign key (manufacturer_id)
         references manufacturer (id)
 );
-alter table product
-    owner to postgres;
 
 alter table users
     alter column password type varchar(100);
@@ -71,3 +62,26 @@ alter table product
 
 alter table manufacturer
     alter column title type varchar(100);
+
+insert into role (title) values ('ROLE_ADMIN');
+insert into role (title) values ('ROLE_USER');
+
+insert into users (email, password, first_name, last_name, role_id)
+values ('admin@mail.com', '$2y$12$MQsWjw7Peb4G9NLaryR9wujZ9OXEC5AYr7uZb8E7TNcC3sK9peDru', 'Admin', 'Admin',
+        (select id from role where title = 'ROLE_ADMIN'));
+insert into users (email, password, first_name, last_name, role_id)
+values ('aushev@mail.com', '$2a$12$1qQpvqztm4/cIYQlhH.eJuPsgxredHKY.bDRlO8r31qGTK5tUnnrq', 'Dima', 'Aushev',
+        (select id from role where title = 'ROLE_USER'));
+
+insert into manufacturer (title) values ('Bromeliaceae');
+insert into manufacturer (title) values ('Marantaceae');
+insert into manufacturer (title) values ('Araceae');
+
+insert into product (title, price, manufacturer_id)
+values ('Stromanthe triostar', 15, (select id from manufacturer where title = 'Marantaceae'));
+insert into product (title, price, manufacturer_id)
+values ('Maranta tricolor', 10, (select id from manufacturer where title = 'Marantaceae'));
+insert into product (title, price, manufacturer_id)
+values ('Philodendron micans', 20, (select id from manufacturer where title = 'Araceae'));
+insert into product (title, price, manufacturer_id)
+values ('Monstera adansonii', 14.95, (select id from manufacturer where title = 'Araceae'));
